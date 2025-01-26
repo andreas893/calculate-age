448fcdc153c7a18deba37f69289bda67109224ff
andreas893 <eaa24andrh@students.eaaa.dk>
2025-01-26 14:16:50 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 "use strict";
+
+const birthYear = prompt("Enter your birthyear:");
+
+const birthYearNum = Number(birthYear);
+
+const currentYear = new Date().getFullYear();
+
+const age = currentYear - birthYearNum;
+
+console.log(`You are ${age} years old`);
